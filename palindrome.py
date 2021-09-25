161e6f9d1d31399f7e86c3b838687f8cca810191
Maxence <mercier.prodev@gmail.com>
2021-09-25 12:08:29 +0200

    refactoring
    add exemples

--- palindrome.py
+++ palindrome.py
@@ -3,11 +3,10 @@ Author : https://github.com/Hatcattt
 
 PalindromePy
 
-This program check if a sentence (or a world) is a palindrome or not.
-Return a boolean True if the sentence is a palindrome, False otherwise.
+This program can check if a sentence (or a word) is a palindrome
+or not with a function called is_palindrome(sentence).
 
 Work with french language too.
-
 """
 
 LETTERS_TO_CHANGE = {
@@ -31,27 +30,41 @@ LETTERS_TO_CHANGE = {
         "'": "",
         "?": "",
         "!": "",
-        ".": "",
-    }
+        ".": ""}
 
 
 def normalize(sentence):
     """
-    This function returns a new sentence in lowercase letters
+    Return a new sentence in lowercase letters
     changing all accented letters to normal letters,
     remove spaces and punctuation marks.
+    :return: string
     """
-    for key, value in LETTERS_TO_CHANGE.items():
-        sentence = sentence.replace(key, value).lower()
+    for char in sentence.lower():
+        if char in LETTERS_TO_CHANGE.keys():
+            sentence = sentence.lower().replace(char, LETTERS_TO_CHANGE[char])
     return sentence
 
 
 def is_palindrome(sentence):
     """
-    This function return True if the string in parameter
+    Return True if the string in parameter
     is a palindrome, False otherwise
-    :return: True or False
+    :return: boolean
     """
     sentence = normalize(sentence)
-    return (sentence == sentence.lower()[::-1]) if len(sentence) > 2 else False
+    return (sentence == sentence[::-1]) if len(sentence) > 2 else False
 
+
+# Exemples:
+print("\nIs it a palindrome?\n")
+
+sentences_to_test = [
+    "stats",
+    "3568653",
+    "I'am a palindrome!",
+    "I did, did I?",
+    "Élu par cette crapule !"]
+
+for example in sentences_to_test:
+    print(is_palindrome(example), ':', example)
